a9de733bbb9cce752a8a000db23e518e834733f6
aycrown1 <ayoadeadedamola198@gmail.com>
2024-03-15 12:59:49 -0700

    File data

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,26 @@ const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
-
+const FilesController = require('../controllers/FilesController');
 
 // Status route
 router.get('/status', AppController.getStatus);
 router.get('/connect', AuthController.getConnect); 
 router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 // Post routes
 router.post('/users', UsersController.postNew);
 
 // Stats route
 router.get('/stats', AppController.getStats);
+
+
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
